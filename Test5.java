59af16672c618643de53516fe22136ea39cddfef
moadamit <moadamit@gmail.com>
2014-07-30 15:28:27 +0530

    commiting test5

---
+++ Test5.java
@@ -0,0 +1,16 @@
+package com.adidas.wc.customs;
+
+import java.util.Enumeration;
+
+import wt.org.OrganizationServicesHelper;
+import wt.util.WTException;
+
+public class Test5 {
+
+	public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+		System.out.println("88888888888888");
+
+	}
+
+}
